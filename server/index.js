934b83e408daeb4f859a680f3e30abb88d985d36
JON J <gr8fuldwg@gmail.com>
2018-11-17 13:27:32 -0500

    updated the forecast coords route to makea  request to darksky and send the resulting data back to the requester

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const axios = require('axios');
 const app = express();
 dotenv.config({ path: __dirname +'/.env'}); //this reads varibales in .env file and
                 // adds them to process
@@ -13,11 +14,19 @@ app.get('/', (request, response) => {
 
 app.get('/forecast/coords/:lat,:lon', (req, res) => {
     const { lat, lon } = req.params;
-    res.status(200);
-    res.json({
-        lat: lat,
-        lon: lon
-    });
+    const url = `https://api.darksky.net/forecast/${apiKey}/${lat},${lon}`;
+    axios.get(url)
+        .then(weatherResponse => {
+            const weather = weatherResponse.data;
+            res.status(200);
+            res.json({
+                weather: weather
+            });
+        })
+    .catch(err => {
+        res.status(err.status || 500);
+        res.send(err.message || "Uh oh");
+        })    
 });
 
 app.listen(port, () => {
