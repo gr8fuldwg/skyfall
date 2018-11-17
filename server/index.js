ecd49585ee65b699bfad70b46a9cc50d615a481a
JON J <gr8fuldwg@gmail.com>
2018-11-17 12:05:36 -0500

    set up evironment variables for the api key and configured dotenv to load envoronemnt variables

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
+dotenv.config({ path: __dirname +'/.env'}); //this reads varibales in .env file and
+                // adds them to process
 const port = process.env.PORT || 4000;
+const apiKey = process.env.API_KEY;
 
 app.get('/', (request, response) => {
     response.status(200);
     response.send('Yayyyyy!!!! It works');
+});
+
+app.get('/forecast/coords/:lat,:lon', (req, res) => {
+    const { lat, lon } = req.params;
+    res.status(200);
+    res.json({
+        lat: lat,
+        lon: lon
+    });
 });
 
 app.listen(port, () => {
