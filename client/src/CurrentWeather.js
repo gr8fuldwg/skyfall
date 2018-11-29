1326755c9fa4dd8cccfc280ee4f38865d08c8ec3
JON J <gr8fuldwg@gmail.com>
2018-11-28 19:00:21 -0500

    tested background color for styling of current weather section

--- client/src/CurrentWeather.js
+++ client/src/CurrentWeather.js
@@ -3,8 +3,19 @@ import React from 'react';
 function CurrentWeather(props) {
     const { apparentTemperature } = props.data
     return (
-        <h1>The temperature is currently: {apparentTemperature} &deg;F</h1>
+
+        <header className="App-header flex navbar-nav " >
+            <h3>Date</h3>
+            <div>
+                <h1>Location</h1>
+
+                <h1>The temperature is currently: {apparentTemperature} &deg;F</h1>
+            </div>
+        </header>
+
     )
 }
+
+
 
 export default CurrentWeather;
