b8fa6eb82ed5e02233e70bba44e2fa45bed39395
JON J <gr8fuldwg@gmail.com>
2018-11-29 20:08:15 -0500

    installed date format and imported to currentWeather. Set up basic display of header

--- client/src/CurrentWeather.js
+++ client/src/CurrentWeather.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
+import format from 'date-fns/format';
+
+const singleHour = hour =>
+    <p key={hour.time}>
+        Temperature: {hour.apparentTemperature}&deg;
+        Time: {new Date(hour.time * 1000).toString()}
+    </p>
 
 function CurrentWeather(props) {
-    const { apparentTemperature } = props.data
+    const { apparentTemperature, time } = props.data
     const { lat, lon } = props
+    const date = new Date(time * 1000)
+
     return (
         <div className="container-fluid">
-
             <div className="row">
                 <div className="col">
-                    <header className="App-header flex-grow" >
-                        <h3>Date</h3>
+                    <header className="App-header flex-grow">
+                        <h2 className="pl-4 pt-4">{format(date, 'MMM DD')}</h2>
                         <div className="h-100">
                             <h3 className="d-flex justify-content-center ">Latitude & Longitude:</h3>
-                            <h1 className="d-flex justify-content-center">{lat} {lon}</h1>
+                            <h1 className="d-flex justify-content-center">{lat.toFixed(2)} {lon.toFixed(2)}</h1>
                             <div className="col-10">
-                                <WeatherIcons className="d-flex justify-content-end mr-5" name="cloud" size="3x" />
+                                <WeatherIcons className="d-flex justify-content-end pb-4 mr-5" name="cloud" size="4x" />
                             </div>
-                            <h1 className="d-flex justify-content-center pb-4">Current Temp: {apparentTemperature} &deg;F</h1>
+                            <h1 className="d-flex justify-content-center pb-4">Current Temp: {apparentTemperature.toFixed(0)} &deg;F</h1>
                         </div>
                     </header>
                 </div>
