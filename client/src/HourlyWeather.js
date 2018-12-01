20f934140b47bc4e6d3a507d4ac15efa6776cf55
JON J <gr8fuldwg@gmail.com>
2018-11-30 21:02:14 -0500

    styled hourly display

--- client/src/HourlyWeather.js
+++ client/src/HourlyWeather.js
@@ -3,24 +3,27 @@ import format from 'date-fns/format';
 
 const singleHour = hour => {
     const date = new Date(hour.time * 1000)
-    return(
-<p key={hour.time}>
-        {hour.apparentTemperature.toFixed(2)}&deg;
-        {format(date, 'h:00 a')}
+    return (
+        <p key={hour.time}>
+            {format(date, ' h:00a ')}
+            {hour.apparentTemperature.toFixed(0)}&deg;
         </p>
     )
 }
-    
+
 
 function HourlyWeather(props) {
     const { data } = props.data;
     const sixHours = data.slice(0, 6);
     const items = sixHours
-    
         .map(singleHour)
     return (
-        <div className="navbar-nav">
-            <h1>{items}</h1>
+        <div className="container-fluid">
+            <div className="navbar-nav">
+                <div className="row">
+                    <h1 className="col-1 justify-center"> {items} </h1>
+                </div>
+            </div>
         </div>
     )
 }
