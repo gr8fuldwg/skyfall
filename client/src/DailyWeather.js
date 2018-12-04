c83db62125c0294370f647b4600d044a125ee7a5
gr8fuldwg <gr8fuldwg@gmail.com>
2018-12-03 20:21:52 -0500

    defined date variable in singleDay function to call date in format object after importing date-fns from format ALLLLLL to display the appropriate DAY in DailyWeather with temp

--- client/src/DailyWeather.js
+++ client/src/DailyWeather.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import format from 'date-fns/format';
 
 const singleDay = day => {
+    const date = new Date(day.time * 1000)
     return (
+        <h1 key={day.time}>
+        {format(date, ' ddd ')}
+        {day.apparentTemperatureMax}&deg;
 
-    <h1 key={day.time}>The first day forecast: {day.apparentTemperatureMax}&deg;</h1>
+    </h1>
     )
 }
 
@@ -11,7 +16,7 @@ function DailyWeather(props) {
     const { data } = props.data;
     const days = data.map(singleDay)
     return (
-        <section>{ days }</section>
+        <section>{days}</section>
     )
 }
 
