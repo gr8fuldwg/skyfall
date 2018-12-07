fc8ec247df600542a780b3872a8f00ecb77307b8
gr8fuldwg <gr8fuldwg@gmail.com>
2018-12-06 21:03:30 -0500

    added icons

--- client/src/CurrentWeather.js
+++ client/src/CurrentWeather.js
@@ -8,12 +8,43 @@ import format from 'date-fns/format';
 //         Time: {new Date(hour.time * 1000).toString()}
 //     </p>
 
+function renderIcon(iconString) {
+    if(iconString === 'cloudy'){
+        return <WeatherIcons name="cloud" size="2x" />
+    }
+    else if(iconString === 'clear-night'){
+        return <WeatherIcons name="night-clear" size="2x" />
+    }
+    else if(iconString === 'clear-day'){
+        return <WeatherIcons name="day-sunny" size="2x" />
+    }
+    else if(iconString === 'rain'){
+        return <WeatherIcons name="rain" size="2x" />
+    }
+    else if(iconString === 'snow'){
+        return <WeatherIcons name="snow" size="2x" />
+    }
+    else if(iconString === 'sleet'){
+        return <WeatherIcons name="sleet" size="2x" />
+    }
+    else {
+        return <WeatherIcons name="windy" size="2x" />
+    }
+}
+
+    // wind, fog, cloudy, 
+    // partly-cloudy-day, or partly-cloudy-night
+
+
 function CurrentWeather(props) {
-    const { apparentTemperature, time } = props.data
+    const { apparentTemperature, time, icon } = props.data
     const { lat, lon } = props
     const date = new Date(time * 1000)
     const { summary } = props.data
     const { temperature } = props.data
+
+    
+    
 
 
     return (
@@ -27,7 +58,8 @@ function CurrentWeather(props) {
                             <h3 className="animated fadeInUp lat-lon d-flex justify-content-center">Latitude & Longitude:</h3>
                             <h1 className="animated flip d-flex justify-content-center pb-5">{lat.toFixed(2)} {lon.toFixed(2)}</h1>
                             <div className="col-7">
-                                <WeatherIcons className="animated fadeInRight d-flex justify-content-end pb-4 mr-5" name="cloud" size="5x" />
+                                <WeatherIcons className="animated fadeInRight d-flex justify-content-end pb-4 mr-5" name="clear" size="5x" />
+                                {renderIcon(icon)}
                             </div>
                             <h1 className="animated slideInUp temp d-flex justify-content-center pb-4">{apparentTemperature.toFixed(0)} &deg;F</h1>
                             <h3 className="d-flex justify-content-center">Real temp {temperature.toFixed(0)} &deg;</h3>
